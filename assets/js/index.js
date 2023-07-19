7bc682fddafa301543a631be83c32b94b70c3c52
rossana mejias <mejiasrossana@gmail.com>
2023-07-19 19:15:39 -0400

    agrego la sección de educacón y experiencia. Estructura, estilo animaciones js

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,5 +10,29 @@
         nav.classList.remove('bg-green', 'shadow');
       }
     });
+
+    const items = document.querySelectorAll("#vertical-scrollable-timeline li");
+
+    function isElementInViewport(el) {
+        var rect = el.getBoundingClientRect();
+        return (
+            rect.top >= 0 &&
+            rect.left >= 0 &&
+            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+        );
+    }
+
+    function callbackFunc() {
+        for (let i = 0; i < items.length; i++) {
+            if (isElementInViewport(items[i])) {
+                items[i].classList.add("active");
+            }
+            else{
+              items[i].classList.remove("active");
+            }
+        }
+    }
+    document.addEventListener('scroll', callbackFunc, true);
 })();
  
